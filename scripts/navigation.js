8940993169efa14920507c5895996001b43e7227
ninafuentes <nfuentesguerrero@byupathway.edu>
2026-09-12 17:59:52 -0600

    w1

---
+++ scripts/navigation.js
@@ -0,0 +1,20 @@
+const menuButton = document.querySelector("#menu-button");
+const navigation = document.querySelector("#navigation");
+
+menuButton.addEventListener("click", () => {
+
+    navigation.classList.toggle("open");
+
+    const isOpen = navigation.classList.contains("open");
+
+    menuButton.setAttribute("aria-expanded", isOpen);
+
+    if (isOpen) {
+        menuButton.textContent = "✕";
+        menuButton.setAttribute("aria-label", "Close navigation menu");
+    } else {
+        menuButton.textContent = "☰";
+        menuButton.setAttribute("aria-label", "Open navigation menu");
+    }
+
+});
